566f972c2316bc8c95b215b588870aa7889506a3
harper25 <olesjakubb@gmail.com>
2023-01-22 18:37:30 +0100

    Extract CSS selectors

--- jjit.py
+++ jjit.py
@@ -9,6 +9,10 @@ import os
 FILENAME = ""
 URL = ""
 
+RAW_OFFERS_DIR = "offers_raw"
+OFFERS_DIR = "offers"
+
+
 from enum import Enum, auto
 from dataclasses import dataclass
 
@@ -19,9 +23,20 @@ class ElementTypeHTML(Enum):
     SPAN = "span"
 
 
-class Attribute(Enum):
+class Attributes(Enum):
     COMPANY_NAME = auto()
     JOB_TITLE = auto()
+    SUMMARY = auto()
+    LOCATION = auto()
+    LOCATION_COMPANY = auto()
+    LOCATION_WORK = auto()
+    SALARY = auto()
+    COMPANY_DETAILS = auto()
+    TECH_STACK = auto()
+    STACKS = auto()
+    TECH = auto()
+    LEVEL = auto()
+    DESCRIPTION = auto()
 
 
 @dataclass
@@ -31,11 +46,19 @@ class Selector:
 
 
 SELECTORS = {
-    Attribute.COMPANY_NAME: Selector(ElementTypeHTML.A.value, "css-l4opor"),
-    "company_name": Selector(ElementTypeHTML.A.value, "css-l4opor"),
-    Attribute.JOB_TITLE: Selector(ElementTypeHTML.DIV.value, "css-1id4k1"),
-    "job_title": Selector(ElementTypeHTML.DIV.value, "css-1id4k1"),
-
+    Attributes.COMPANY_NAME: Selector(ElementTypeHTML.A.value, "css-l4opor"),
+    Attributes.JOB_TITLE: Selector(ElementTypeHTML.DIV.value, "css-1id4k1"),
+    Attributes.SUMMARY: Selector(ElementTypeHTML.DIV.value, "css-1kgdb8a"),
+    Attributes.LOCATION: Selector(ElementTypeHTML.DIV.value, "css-1f4p1d3"),
+    Attributes.LOCATION_COMPANY: Selector(ElementTypeHTML.SPAN.value, "css-9wmrp4"),
+    Attributes.LOCATION_WORK: Selector(ElementTypeHTML.SPAN.value, "css-13p5d07"),
+    Attributes.SALARY: Selector(ElementTypeHTML.DIV.value, "css-1wla3xl"),
+    Attributes.COMPANY_DETAILS: Selector(ElementTypeHTML.DIV.value, "css-1ji7bvd"),
+    Attributes.TECH_STACK: Selector(ElementTypeHTML.DIV.value, "css-1ikoimk"),
+    Attributes.STACKS: Selector(ElementTypeHTML.DIV.value, "css-1q98d5e"),
+    Attributes.TECH: Selector(ElementTypeHTML.DIV.value, "css-1eroaug"),
+    Attributes.LEVEL: Selector(ElementTypeHTML.DIV.value, "css-19mz16e"),
+    Attributes.DESCRIPTION: Selector(ElementTypeHTML.DIV.value, "css-p1hlmi"),
 }
 
 async def download_page():
@@ -47,7 +70,7 @@ async def download_page():
 
     # propose filename & wait for input
 
-    with open(FILENAME, "w") as file:
+    with open(os.path.join(RAW_OFFERS_DIR, FILENAME), "w") as file:
         file.write(page_content)
 
     await browser.close()
@@ -56,28 +79,14 @@ async def download_page():
 asyncio.get_event_loop().run_until_complete(download_page())
 
 
-def get_attribute(soup, attribute):
-    element = soup.find(SELECTORS[attribute].html, class_=SELECTORS[attribute].class_)
+def get_attribute(soup, attribute, selectors=SELECTORS):
+    element = soup.find(selectors[attribute].html, class_=selectors[attribute].class_)
     return element
 
 
-def get_company_name(soup):
-    company_name = soup.find(
-        name= SELECTORS[Attribute.COMPANY_NAME].html,
-        class_=SELECTORS[Attribute.COMPANY_NAME].class_
-    )
-    # company_name = soup.find(SELECTORS["company_name"].html, class_=SELECTORS["company_name"].class_)
-    return company_name.get_text()
-
-
-def get_job_title(soup):
-    job_title = soup.find(SELECTORS["job_title"].html, class_=SELECTORS["job_title"].class_)
-    return job_title.get_text()
-
-
-def get_job_title2(soup):
-    job_title = get_attribute(soup, Attribute.JOB_TITLE)
-    return job_title.get_text()
+def get_attributes(soup, attribute, selectors=SELECTORS):
+    element = soup.find_all(selectors[attribute].html, class_=selectors[attribute].class_)
+    return element
 
 
 def get_current_date():
@@ -87,14 +96,15 @@ def get_current_date():
 
 def generate_filename(soup):
     today = get_current_date()
-    company_name = get_company_name(soup)
-    job_title = get_job_title(soup)
-    return f"{today} {company_name} - {job_title}"
+    company_name = get_attribute(soup, Attributes.COMPANY_NAME)
+    job_title = get_attribute(soup, Attributes.JOB_TITLE)
+
+    return f"{today} {company_name.get_text()} - {job_title.get_text()}"
 
 
 
 def parse():
-    with open(FILENAME, "r") as file:
+    with open(os.path.join(RAW_OFFERS_DIR, FILENAME), "r") as file:
         page_content = file.read()
 
     soup = BeautifulSoup(page_content, features="html.parser")
@@ -104,54 +114,50 @@ def parse():
     print(debug_filename)
     print("-"*len(debug_filename))
 
-    job_title = get_attribute(soup, "job_title")
-    print(job_title.get_text())
-    job_title = get_attribute(soup, Attribute.JOB_TITLE)
+    job_title = get_attribute(soup, Attributes.JOB_TITLE)
     print(job_title.get_text())
 
     # import sys
     # sys.exit(0)
 
-
-    job_title = soup.find("div", class_="css-1id4k1")
-    print(job_title.get_text())
-
-    summary_soup = soup.find("div", class_="css-1kgdb8a")
-    company_name = summary_soup.find("a", class_="css-l4opor")
+    summary_soup = get_attribute(soup, Attributes.SUMMARY)
+    # print(summary_soup.get_text())
+    company_name = get_attribute(summary_soup, Attributes.COMPANY_NAME)
     print(company_name.get_text())
     print(company_name["href"])
     print()
 
-    location_soup = soup.find("div", class_="css-1f4p1d3")
+    location_soup = get_attribute(soup, Attributes.LOCATION)
+
     # print("Location soup:", location_soup)
-    company_location = location_soup.find("span", class_="css-9wmrp4")
-    working_location = location_soup.find("span", class_="css-13p5d07")
+    company_location = get_attribute(location_soup, Attributes.LOCATION_COMPANY)
+    working_location = get_attribute(location_soup, Attributes.LOCATION_WORK)
     print(company_location.get_text())
     if working_location:
         print(working_location.get_text())
     print()
 
-    salary_soups = soup.find_all("div", class_="css-1wla3xl")
+    salary_soups = get_attributes(soup, Attributes.SALARY)
     for salary_soup in salary_soups:
         print(salary_soup.get_text())
 
     print()
-    company_details = summary_soup.find_all("div", class_="css-1ji7bvd")
+    company_details = get_attributes(soup, Attributes.COMPANY_DETAILS)
     for desc, item in zip(["Company Size:", "Level:"], company_details):
         print(desc, item.get_text().strip())
     print()
 
     # print("Tech stack")
-    tech_stack_soup = soup.find("div", class_="css-1ikoimk")
+    tech_stack_soup = get_attribute(soup, Attributes.TECH_STACK)
     # print(tech_stack_soup)
-    stack_soups = tech_stack_soup.find_all("div", class_="css-1q98d5e")
+    stack_soups = get_attributes(tech_stack_soup, Attributes.STACKS)
     for stack_soup in stack_soups:
-        tech = stack_soup.find("div", class_="css-1eroaug")
-        level = stack_soup.find("div", class_="css-19mz16e")
+        tech = get_attribute(stack_soup, Attributes.TECH)
+        level = get_attribute(stack_soup, Attributes.LEVEL)
         print(f"{tech.get_text()}: {level.get_text()}")
 
     print()
-    details = soup.find("div", class_="css-p1hlmi")
+    details = get_attribute(soup, Attributes.DESCRIPTION)
     # print(details.get_text())
 
 
