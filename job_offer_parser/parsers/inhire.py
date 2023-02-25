b37f4c8ac526c571ef906ad7611a4e6c95a7928b
harper25 <olesjakubb@gmail.com>
2023-02-25 18:27:09 +0100

    Add template parser for inhire

---
+++ job_offer_parser/parsers/inhire.py
@@ -0,0 +1,55 @@
+from enum import auto
+from typing import Dict, List, Optional
+
+from job_offer_parser.parsers.base_parser import (
+    AutoNameEnum,
+    ElementTypeHTML,
+    Parser,
+    Selector,
+)
+
+
+class InhireAttributes(AutoNameEnum):
+    COMPANY_NAME = auto()
+    JOB_TITLE = auto()
+
+
+INHIRE_SELECTORS = {
+    InhireAttributes.COMPANY_NAME.value: Selector(ElementTypeHTML.A.value, "inh-link"),
+    InhireAttributes.JOB_TITLE.value: Selector(
+        ElementTypeHTML.H1.value,
+        "inh-offer-view-header__offer-name inh-text-header inh-text-header--white inh-text-header--bold",  # noqa E501
+    ),
+}
+
+
+class InhireParser(Parser):
+    def __init__(
+        self,
+        text: str,
+        selectors: Optional[Dict[str, Selector]] = None,
+        attributes: Optional[InhireAttributes] = None,
+    ) -> None:
+        selectors = selectors or INHIRE_SELECTORS
+        attrs = attributes or InhireAttributes
+        self._attributes = attrs
+        super().__init__(text, selectors)
+
+    @staticmethod
+    def meets_condition(source: str) -> bool:
+        return "inhire" in source
+
+    def get_company_name(self) -> str:
+        company_name = self.get_attribute(
+            self._attributes.COMPANY_NAME.value
+        ).get_text()
+        company_name = " ".join(company_name.split())
+        return company_name
+
+    def get_job_title(self) -> str:
+        job_title = self.get_attribute(self._attributes.JOB_TITLE.value).get_text()
+        job_title = " ".join(job_title.split())
+        return job_title
+
+    def parse(self) -> List[str]:
+        return ["Parsing will be implemented later..."]
