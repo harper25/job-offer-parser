d818d16e0df9a31f8ad1cdaaa93ccffdc2f77790
harper25 <olesjakubb@gmail.com>
2023-10-07 21:18:06 +0200

    Extract Selectors for parsing

--- job_offer_parser/parsers/base_parser.py
+++ job_offer_parser/parsers/base_parser.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import StrEnum, auto
 from typing import Any, Dict, List, Optional, Protocol, Union
 
 from bs4 import BeautifulSoup, PageElement, Tag
 from job_offer_parser.module_exceptions import AttributeNotFoundError
 
 
-class AutoNameEnum(Enum):
+class AutoNameEnum(StrEnum):
     @staticmethod
     def _generate_next_value_(
         name: str, start: int, count: int, last_values: List[Any]
@@ -15,11 +15,12 @@ class AutoNameEnum(Enum):
         return name.lower()
 
 
-class ElementTypeHTML(AutoNameEnum):
+class HTMLTag(AutoNameEnum):
     A = auto()
     DIV = auto()
     H1 = auto()
     H2 = auto()
+    H6 = auto()
     SPAN = auto()
 
 
@@ -29,13 +30,24 @@ class Selector:
     class_: str
 
 
+class Selectors:
+    def __init__(self, selectors: Dict[str, Selector]) -> None:
+        self._selectors = selectors
+
+    def get_html_tags(self, attribute: str) -> Selector:
+        return self._selectors[attribute]
+
+
+class BaseAttributes(AutoNameEnum):
+    COMPANY_NAME = auto()
+    JOB_TITLE = auto()
+
+
 class Parser(ABC):
     portal_identifier = ""
 
-    def __init__(self, text: str, selectors: Dict[str, Selector]) -> None:
+    def __init__(self, text: str) -> None:
         self._text = text
-        self._selectors = selectors
-        self._soup = BeautifulSoup(self._text, features="html.parser")
 
     @abstractmethod
     def get_company_name(self) -> str:
@@ -49,23 +61,46 @@ class Parser(ABC):
     def meets_condition(cls, portal: str) -> bool:
         return cls.portal_identifier in portal if cls.portal_identifier else False
 
+    @abstractmethod
+    def parse(self) -> List[str]:
+        pass
+
+
+class BaseParser(Parser):
+    def __init__(self, text: str, selectors: Selectors) -> None:
+        self._text = text
+        self._selectors = selectors
+        self._soup = BeautifulSoup(self._text, features="html.parser")
+
     def get_attribute(
         self, attribute: str, soup: Optional[Union[BeautifulSoup, Tag]] = None
     ) -> Tag:
         soup = soup or self._soup
-        selector = self._selectors[attribute]
+        selector = self._selectors.get_html_tags(attribute)
         element = soup.find(selector.html, class_=selector.class_)
         if not isinstance(element, Tag):
-            raise AttributeNotFoundError
+            raise AttributeNotFoundError(
+                f"Attribute not found with parser: {self.__class__.__name__}"
+            )
         return element
 
     def get_attributes(
         self, attribute: str, soup: Optional[Union[BeautifulSoup, Tag]] = None
     ) -> List[PageElement]:
         soup = soup or self._soup
-        selector = self._selectors[attribute]
+        selector = self._selectors.get_html_tags(attribute)
         element = soup.find_all(selector.html, class_=selector.class_)
         return element
+
+    def get_company_name(self) -> str:
+        company_name = self.get_attribute(BaseAttributes.COMPANY_NAME.value).get_text()
+        company_name = " ".join(company_name.split())
+        return company_name
+
+    def get_job_title(self) -> str:
+        job_title = self.get_attribute(BaseAttributes.JOB_TITLE.value).get_text()
+        job_title = " ".join(job_title.split())
+        return job_title
 
     def parse(self) -> List[str]:
         return ["Parsing will be implemented later..."]
